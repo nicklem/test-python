751abe7ba3799b85f33233197089e4b7877b86d5
nicklem <nicola.klemenc@codacy.com>
2023-02-08 13:52:11 +0100

    init

---
+++ some_code.py
@@ -0,0 +1,16 @@
+def add_one(x):
+    return x + 1
+
+
+def add_two(x):
+    return x + 2
+
+# Tests
+
+
+def test_add_one():
+    assert add_one(1) == 2
+
+
+# def test_add_two():
+#     assert add_two(1) == 3
